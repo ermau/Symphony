8eab2b7a86bae1316474c63406aaa9d07cd52b6c
ermau <me@ermau.com>
2011-06-20 23:33:41 -0400

    Improved Ogg support

--- Desktop/Symphony.Xiph/Ogg/OggParser.cs
+++ Desktop/Symphony.Xiph/Ogg/OggParser.cs
@@ -23,6 +23,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using Symphony.Encoding;
 
@@ -39,7 +40,181 @@ namespace Symphony.Xiph.Ogg
 			this.stream = stream;
 		}
 
+		private bool continuation;
+		private int lastStreamIndex;
+		private readonly Dictionary<int, OggStream> oggStreams = new Dictionary<int, OggStream>();
+
 		public bool TryReadPacket (out Packet packet)
+		{
+			//Trace.WriteLine ("Entering", "TryReadPacket");
+			//Trace.Indent();
+
+			packet = null;
+
+			int lastSegment = 0;
+			OggPage page, firstPage;
+			OggStream ostream;
+			if (!this.continuation)
+			{
+				//Trace.WriteLine ("Not a continuation, reading page", "TryReadPacket");
+
+				firstPage = page = ReadPage();
+				if (page == null)
+				{
+					//Trace.Unindent();
+					//Trace.WriteLine ("Exiting (no packet)", "TryReadPacket");
+					return false;
+				}
+
+				if (!this.oggStreams.TryGetValue (page.StreamIndex, out ostream))
+				{
+					//Trace.WriteLine ("New ogg stream: " + page.StreamIndex, "TryReadPacket");
+					this.oggStreams[page.StreamIndex] = ostream = new OggStream (ReadPage);
+				}
+				
+				//Trace.WriteLine (String.Format("SI: {0}; G: {1:N0}; S: {2:N0}; SL: {3:N0}", page.StreamIndex, page.GranuleNumber, page.SequenceNumber, page.Segments.Length));
+
+				ostream.Enqueue (page);
+			}
+			else
+			{
+				//Trace.WriteLine ("Continuing on page from " + this.lastStreamIndex, "TryReadPacket");
+
+				ostream = this.oggStreams[this.lastStreamIndex];
+				if ((firstPage = page = ostream.Peek()) == null && (firstPage = page = ReadPage()) == null)
+				{
+					//Trace.Unindent();
+					//Trace.WriteLine ("Exiting (no page)", "TryReadPacket");
+					return false;
+				}
+
+				lastSegment = ostream.LastSegment;
+				//Trace.WriteLine (this.lastStreamIndex + " Last segment: " + lastSegment, "TryReadPacket");
+			}
+
+			List<byte[]> segments = new List<byte[]>();
+
+			int packetLength = 0;
+			bool packetEndFound = false;
+			while (!packetEndFound)
+			{
+				int i;
+				for (i = lastSegment; i < page.Segments.Length; ++i)
+				{
+					int length = page.Segments[i].Length;
+					packetLength += length;
+					segments.Add (page.Segments[i]);
+
+					//Trace.WriteLine (String.Format ("Segment {0:N0}, length: {1}", i, length), "TryReadPacket");
+					if (length < 255)
+					{
+						packetEndFound = true;
+						i++;
+						break;
+					}
+				}
+
+				if (!packetEndFound)
+				{
+					//Trace.WriteLine ("Reached end of page, continuing with another", "TryReadPacket");
+					ostream.Dequeue(); // used up whole last page
+
+					OggPage nextPage;
+					while (true)
+					{
+						if ((nextPage = ostream.Peek()) == null && (nextPage = ReadPage()) == null)
+						{
+							this.continuation = true; // Let's try this stream again next call.
+							this.lastStreamIndex = page.StreamIndex;
+							ostream.Enqueue (page);
+							//Trace.Unindent();
+							//Trace.WriteLine ("Exiting (no page to continue with)", "TryReadPacket");
+							return false;
+						}
+
+						if (nextPage.StreamIndex != page.StreamIndex)
+						{
+							OggStream nextStream;
+							if (!this.oggStreams.TryGetValue (page.StreamIndex, out nextStream))
+							{
+								//Trace.WriteLine ("New ogg stream: " + page.StreamIndex, "TryReadPacket");
+								this.oggStreams[page.StreamIndex] = nextStream = new OggStream (ReadPage);
+							}
+
+							nextStream.Enqueue (page);
+						}
+						else
+						{
+							ostream.Enqueue (page);
+							break;
+						}
+					}
+
+					page = nextPage;
+					lastSegment = 0;
+				}
+				else
+				{
+					ostream.PacketCount++;
+
+					if (i != page.Segments.Length)
+					{
+						//Trace.WriteLine ("Not done with this page yet (" + i + "/" + page.Segments.Length + ")", "TryReadPacket");
+						this.continuation = true;
+						this.lastStreamIndex = page.StreamIndex;
+						ostream.LastSegment = i;
+					}
+					else
+					{
+						//Trace.WriteLine ("Finished packet", "TryReadPacket");
+						ostream.Dequeue();
+						this.continuation = false;
+					}
+				}
+			}
+
+			byte[] pdata = AssemblePacket (segments);
+			packet = new OggPacket (page.StreamIndex, ostream.PacketCount - 1, firstPage.GranuleNumber, pdata);
+
+			//Trace.Unindent();
+			//Trace.WriteLine ("Exiting", "TryReadPacket");
+
+			return true;
+		}
+
+		private class OggStream
+		{
+			private readonly Func<OggPage> nextPage;
+
+			public OggStream (Func<OggPage> nextPage)
+			{
+				if (nextPage == null)
+					throw new ArgumentNullException ("nextPage");
+				this.nextPage = nextPage;
+			}
+			
+			public int LastSegment;
+			public int PacketCount;
+
+			public OggPage Peek()
+			{
+				return (this.lastPages.Count != 0) ? this.lastPages.Peek() : null;
+			}
+
+			public void Enqueue (OggPage page)
+			{
+				this.lastPages.Enqueue (page);
+			}
+
+			public void Dequeue()
+			{
+				this.lastPages.Dequeue();
+			}
+
+			private readonly Queue<OggPage> lastPages = new Queue<OggPage>();
+		}
+
+		/*public bool TryReadPacket (out Packet packet)
 		{
 			packet = null;
 
@@ -110,14 +285,14 @@ namespace Symphony.Xiph.Ogg
 
 				pagesOfData.Add (packetData);
 
-				this.offset = (i + 1 == page.Segments.Length) ? 0 : i + 1;
+				this.offset = (i + 1 >= page.Segments.Length) ? 0 : i + 1;
 			}
 
 			if (havePacket)
 				packet = new OggPacket (streamIndex, 0, this.lastPage.GranuleNumber, AssemblePacket (pagesOfData));
 
 			return havePacket;
-		}
+		}*/
 
 		public bool TryReadContext (out IFormatContext context)
 		{
@@ -128,7 +303,7 @@ namespace Symphony.Xiph.Ogg
 		private OggPage lastPage;
 		private readonly Stream stream;
 
-		private Queue<OggPage> pageBuffer = new Queue<OggPage>();
+		private readonly Queue<OggPage> pageBuffer = new Queue<OggPage>();
 
 		private byte[] AssemblePacket (List<byte[]> pages)
 		{
