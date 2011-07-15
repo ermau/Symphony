3c27d4606ad28433a4208e8db7529e9805378eb2
ermau <me@ermau.com>
2011-07-15 18:45:53 -0400

    Add Windows XNA library

---
+++ Desktop/Symphony.Xna/Vector3Extensions.cs
@@ -0,0 +1,38 @@
+﻿//
+// MIT License
+// Copyright ©2011 Eric Maupin
+// All rights reserved.
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+namespace Symphony.Xna
+{
+	public static class Vector3Extensions
+	{
+		public static Symphony.Vector3 ToSymphonyVector3 (this Microsoft.Xna.Framework.Vector3 self)
+		{
+			return new Symphony.Vector3 (self.X, self.Y, self.Z);
+		}
+
+		public static Microsoft.Xna.Framework.Vector3 ToXnaVector3 (this Symphony.Vector3 self)
+		{
+			return new Microsoft.Xna.Framework.Vector3 (self.X, self.Y, self.Z);
+		}
+	}
+}
